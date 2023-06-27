5ad759a65b3c72207a6c19afbfe7a06ef092e52d
GLCVLL <g.ioele96@outlook.it>
2023-06-27 18:51:54 +0200

    created 10  random emails

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-console.log('js ok');
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-console.log('vue ok', Vue);
+// creo app
+const app = Vue.createApp({
+    data() {
+        return {
+            emails: [],
+        }
+    },
+    methods: {
+        getRandomEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(endpoint)
+                .then((res) => {
+                    const myemails = res.data.response;
+                    this.emails.push(myemails);
+                })
+            };
+        },
+    },
+    created() {
+        this.getRandomEmails()
+    }
+
+});
+
+app.mount('#root');
